b665d28be352ff4a75b51364dde05d349454cdf6
Simon Pettersson <simon.v.pettersson@gmail.com>
2017-05-12 17:57:37 +0200

    Added more tests

--- test/cmap.cpp
+++ test/cmap.cpp
@@ -4,9 +4,49 @@
 using cmap::make_map;
 using cmap::map;
 
-SCENARIO("Helo") {
-  constexpr auto lookup = make_map(map(1,2), map(2,3), map(5,7));
-  CHECK( lookup[1] == 2 );
-  CHECK( lookup[2] == 3 );
-  CHECK( lookup[5] == 7 );  
+SCENARIO("Compile time mapping") {
+  GIVEN("a map of int -> int") {
+    constexpr auto lookup = make_map(
+        map(1,2),
+        map(2,3),
+	map(5,7)
+    );
+    THEN("chek each mapping") {
+      CHECK( lookup[1] == 2 );
+      CHECK( lookup[2] == 3 );
+      CHECK( lookup[5] == 7 );
+    }
+  }
+
+  GIVEN("a custom type") {
+    struct MyType {
+      constexpr bool operator==(const MyType& rhs) const { return value == rhs.value; }
+      const int value;
+    };
+    GIVEN("a map of int -> custom type") {
+      constexpr auto lookup = make_map(
+          map(42, MyType{12}),
+          map(43, MyType{13}),
+          map(44, MyType{14})
+      );
+      THEN("check each mapping") {
+	CHECK( lookup[42] == MyType{12} );
+	CHECK( lookup[43] == MyType{13} );
+	CHECK( lookup[44] == MyType{14} );
+      }
+    }
+
+    GIVEN("a map of custom type -> int") {
+      constexpr auto lookup = make_map(
+          map(MyType{12}, 42),
+          map(MyType{13}, 43),
+          map(MyType{14}, 44)
+      );
+      THEN("check each mapping") {
+        CHECK( lookup[MyType{12}] == 42 );
+        CHECK( lookup[MyType{13}] == 43 );
+        CHECK( lookup[MyType{14}] == 44 );
+      }
+    }
+  }
 }
